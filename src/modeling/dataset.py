68b84d6c61be43f3ab4b966f352895f4c9fb2c7a
Zayed Ahmed <zayed967@yahoo.com>
2026-02-12 13:58:49 +0300

    few touches

--- src/modeling/dataset.py
+++ src/modeling/dataset.py
@@ -9,7 +9,9 @@ def get_hourly_df():
         engine = get_engine()
 
         returns_query = """
-            SELECT * FROM returns_1h ORDER BY time;
+            SELECT symbol, time, close, r
+            FROM returns_1h
+            ORDER BY time;
         """
 
         with engine.begin() as conn:
@@ -17,23 +19,22 @@ def get_hourly_df():
 
             data = result.fetchall()
 
-            if data:
-                df = pd.DataFrame(data, columns=result.keys())
-                df.dropna()
-                df["y"] = df["r"].shift(-1)
-                df["lag0"] = df["r"].shift(0)
-                df["lag1"] = df["r"].shift(1)
-                df["lag2"] = df["r"].shift(2)
-                df["lag3"] = df["r"].shift(3)
+            if not data:
+                return None
 
-                return df
-            
-            else:
-                raise Exception(f"\nNo data found in the database")
+            df = pd.DataFrame(data, columns=result.keys())
+            df["y"] = df["r"].shift(-1)
+            df["lag0"] = df["r"].shift(0)
+            df["lag1"] = df["r"].shift(1)
+            df["lag2"] = df["r"].shift(2)
+            df["lag3"] = df["r"].shift(3)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+            return df.dropna(subset=["y", "lag0", "lag1", "lag2", "lag3"])
+
+    except Exception:
+        raise
 
 
 if __name__ == "__main__":
-    get_hourly_df()
+    df = get_hourly_df()
+    print(df.head())
