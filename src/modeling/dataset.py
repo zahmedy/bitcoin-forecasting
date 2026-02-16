b9de2eb26aa9cac2a7879c1884cbb4943a8b643c
Zayed Ahmed <zayed967@yahoo.com>
2026-02-16 11:06:53 +0300

    make it abs

--- src/modeling/dataset.py
+++ src/modeling/dataset.py
@@ -2,7 +2,6 @@ from src.db.db import get_engine
 from sqlalchemy import text
 import pandas as pd
 
-
 def get_hourly_df():
     
     try:
@@ -23,17 +22,20 @@ def get_hourly_df():
                 return None
 
             df = pd.DataFrame(data, columns=result.keys())
-            df["y"] = df["r"].shift(-1)
-            df["lag0"] = df["r"].shift(0)
-            df["lag1"] = df["r"].shift(1)
-            df["lag2"] = df["r"].shift(2)
-            df["lag3"] = df["r"].shift(3)
+            df["abs_r"] = df["r"].abs()
+            df["y"] = df["abs_r"].shift(-1)
+            df["lag0"] = df["abs_r"].shift(0)
+            df["lag1"] = df["abs_r"].shift(1)
+            df["lag2"] = df["abs_r"].shift(2)
+            df["lag3"] = df["abs_r"].shift(3)
 
-            return df.dropna(subset=["y", "lag0", "lag1", "lag2", "lag3"])
-
+            return df.dropna(subset=["y", 
+                                     "lag0", 
+                                     "lag1", 
+                                     "lag2", 
+                                     "lag3"])
     except Exception:
         raise
-
 
 if __name__ == "__main__":
     df = get_hourly_df()
